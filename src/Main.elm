498c959d102ba0cff0cbb29a42be78f3227610f9
cpkmn <markovdmitry99@yandex.ru>
2024-07-23 11:30:27 +0300

    wip ruler

--- src/Main.elm
+++ src/Main.elm
@@ -14,70 +14,50 @@ type Quadtree a
 
 
 
--- TODO: scale, repr
+-- TODO: scale
 
 
-viewPixel color size =
+viewPixel color n maxSize =
+    let
+        sizeStr =
+            String.fromFloat (maxSize / (2 ^ n)) ++ "px"
+    in
     div
-        [ style "width" "100%"
-        , style "height" "100%"
+        [ style "width" sizeStr
+        , style "height" sizeStr
         , style "background-color" color
         ]
         [ text "\u{200B}" ]
 
 
-viewQuadtreePixel color size =
-    td [ style "padding" "0" ]
-        [ viewPixel color size ]
-
-
-
--- viewQuadtree x =
---     case x of
---         QuadEmpty ->
---             viewPixel "#FFF"
---         QuadLeaf color ->
---             viewPixel color
---         QuadNode node ->
---             table
---                 [ style "border-collapse" "collapse" ]
---                 [ tbody []
---                     [ tr [] [ viewQuadtree node.tl, viewQuadtree node.tr ]
---                     , tr [] [ viewQuadtree node.bl, viewQuadtree node.br ]
---                     ]
---                 ]
-
-
-viewQuadtree0 x n =
-    let
-        size =
-            1 / n
-    in
+viewQuadtree0 x maxSize n =
     case x of
         QuadEmpty ->
-            viewPixel "#FFF" size
+            viewPixel "#FFF" n maxSize
 
         QuadLeaf color ->
-            viewPixel color size
+            viewPixel color n maxSize
 
         QuadNode node ->
             table
-                [ style "border-collapse" "collapse" ]
+                [ style "border-collapse" "collapse"
+                , style "z-index" "0"
+                ]
                 [ tbody []
                     [ tr []
-                        [ td [ style "padding" "0" ] [ viewQuadtree node.tl ]
-                        , td [ style "padding" "0" ] [ viewQuadtree node.tr ]
+                        [ td [ style "padding" "0" ] [ viewQuadtree0 node.tl maxSize (n + 1) ]
+                        , td [ style "padding" "0" ] [ viewQuadtree0 node.tr maxSize (n + 1) ]
                         ]
                     , tr []
-                        [ td [ style "padding" "0" ] [ viewQuadtree node.bl ]
-                        , td [ style "padding" "0" ] [ viewQuadtree node.br ]
+                        [ td [ style "padding" "0" ] [ viewQuadtree0 node.bl maxSize (n + 1) ]
+                        , td [ style "padding" "0" ] [ viewQuadtree0 node.br maxSize (n + 1) ]
                         ]
                     ]
                 ]
 
 
-viewQuadtree x =
-    div [ style "width" "100px", style "height" "100px" ] [ viewQuadtree0 x 1 ]
+viewQuadtree x maxSize =
+    viewQuadtree0 x maxSize 0
 
 
 quad0 =
@@ -95,12 +75,61 @@ quad0 =
         }
 
 
-test =
-    quad0
+
+-- scale 3 = 3x3
+
+
+viewRuler scale maxSize =
+    let
+        n =
+            2 ^ scale
+
+        sizeStr =
+            String.fromFloat (maxSize / n) ++ "px"
+    in
+    table
+        [ style "border-collapse" "collapse"
+        , style "border-right" "1px solid #333"
+        , style "border-bottom" "1px solid #333"
+        , style "z-index" "1"
+        , style "box-sizing" "border-box"
+        ]
+        [ tbody []
+            (List.repeat (round n)
+                (tr []
+                    (List.repeat (round n)
+                        (td
+                            [ style "padding" "0"
+                            , style "border-left" "1px solid #333"
+                            , style "border-top" "1px solid #333"
+                            , style "box-sizing" "border-box"
+                            ]
+                            [ div
+                                [ style "width" sizeStr
+                                , style "height" sizeStr
+
+                                --style "background-color" "transp"
+                                ]
+                                [ text "\u{200B}" ]
+                            ]
+                        )
+                    )
+                )
+            )
+        ]
 
 
 main =
-    div []
-        [ viewQuadtree test
-        , text (Debug.toString test)
+    div
+        [ style "position" "absolute"
+        , style "top" "10px"
+        , style "left" "10px"
+
+        -- , style "box-sizing" "border-box"
+        -- , style "-moz-box-sizing" "border-box"
+        -- , style "-webkit-box-sizing" "border-box"
+        ]
+        [ viewRuler 3 256
+        , viewQuadtree quad0 256
+        , text (Debug.toString quad0)
         ]
